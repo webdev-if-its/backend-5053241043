e376681777c83c0ff0f4e79b2ce9af2fc2f837f6
Muhammad Alfaraldi Raihan <muhammadaldi2236@gmail.com>
2026-09-15 17:24:38 +0700

    level 7 done

--- pertemuan-03/main.go
+++ pertemuan-03/main.go
@@ -70,7 +70,16 @@ func HapusTugas(toko *TokoTugas, id int) error {
 // HapusTugasTercatat memanggil HapusTugas, lalu memakai defer untuk
 // MENCATAT hasilnya ke toko.Log -- baik saat berhasil maupun saat gagal.
 func HapusTugasTercatat(toko *TokoTugas, id int) error {
-	panic("belum diimplementasikan")
+	var err error
+	defer func() {
+		if err != nil {
+			toko.Log = append(toko.Log, fmt.Sprintf("hapus id=%d: gagal (%v)", id, err))
+		} else {
+			toko.Log = append(toko.Log, fmt.Sprintf("hapus id=%d: berhasil", id))
+		}
+	}()
+	err = HapusTugas(toko, id)
+	return err
 }
 
 // AmankanPanggilan menjalankan fn. Kalau fn panic, AmankanPanggilan
